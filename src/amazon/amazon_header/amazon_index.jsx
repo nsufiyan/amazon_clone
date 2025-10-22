05fc7a778de001f5212b70dc0dd506ce31642cbb
nsufiyan <shaiknizamuddinsufiyan@gmail.com>
2025-10-22 17:25:28 +0530

    changes in index

--- src/amazon/amazon_header/amazon_index.jsx
+++ src/amazon/amazon_header/amazon_index.jsx
@@ -5,19 +5,20 @@ import { Amazon_Home } from "./amazon_home";
 import { Amazon_SingIn } from "./amazon_sing_up";
 
 
-export function  Amazon_Index(){
+export function Amazon_Index() {
 
 
-    return(
+
+  return (
     <div>
       <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Amazon_Home/>} />
-          <Route path="home" element={<Amazon_Home/>} />
-          <Route path="singin" element={<Amazon_SingIn/>}/>
-      </Routes>
-      
-      </BrowserRouter>    
+        <Routes>
+          <Route path="/" element={<Amazon_Home />} />
+          <Route path="home" element={<Amazon_Home />} />
+          <Route path="singin" element={<Amazon_SingIn />} />
+        </Routes>
+
+      </BrowserRouter>
     </div>
-    )
+  )
 }
