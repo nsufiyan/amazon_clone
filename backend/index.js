fe0a3d88a15faecc5a393dcde3618e5671418878
nsufiyan <shaiknizamuddinsufiyan@gmail.com>
2025-11-05 00:02:33 +0530

    server file

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,29 @@
-const express = require("express");
-const server = express();
-const PORT = process.env.PORT
 
-//amazon
-server.listen(PORT, () => {
-    console.log(`server started at ${PORT}`)
+var express = require("express");
+var cors = require("cors");
+
+var app = express();
+app.use(cors());
+const path = require('path')
+var mongoose = require("mongoose");
+const amazonRouter = require("./Router/amazonRouter");
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/media')));
+
+
+//connecting to db
+mongoose.connect('mongodb://localhost:27017/Amazon')
+    .then(() => {
+        console.log("Connected to Amazon database successfully")
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.use("/api", amazonRouter);
+
+
+app.listen(5050, () => {
+    console.log("Server is started at https:127.0.0.1:5050")
 })
+
