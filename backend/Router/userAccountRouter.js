1c2578f334b8e059a667a0d2d3d0a4a5fdf3b196
nsufiyan <shaiknizamuddinsufiyan@gmail.com>
2025-11-17 17:24:49 +0530

    router

--- backend/Router/userAccountRouter.js
+++ backend/Router/userAccountRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const userRouter = express.Router();
-const { add, get, deleteAcc } = require("../Controller/userAccountController")
+const { add, get, deleteAcc, update } = require("../Controller/userAccountController")
 
-
-userRouter.get("/get-user", get);
+const { checkSession } = require("../util/sessionCheck")
+userRouter.post("/get-user", get);
 userRouter.post("/login-user", add);
 userRouter.delete("/delete-user", deleteAcc);
+userRouter.put("/update-user", update);
 
 module.exports = userRouter;
